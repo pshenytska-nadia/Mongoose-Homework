618669c4a750d5b11346850fb43ae50f48393f16
Nadiia Pshenytska <nadiya.pshenytska@gmail.com>
2023-12-08 14:43:12 +0200

    created user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,69 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+	firstName: {
+		type: String,
+		minLength: 4,
+		maxLength: 50,
+		required: true,
+		trim: true,
+	},
+	lastName: {
+		type: String,
+		minLength: 3,
+		maxLength: 60,
+		required: true,
+		trim: true,
+	},
+	fullName: {
+		type: String,
+	},
+	email: {
+		type: String,
+		required: true,
+		lowercase: true,
+		unique: true,
+		match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email format'],
+	},
+	role: {
+		type: String,
+		enum: ['admin', 'writer', 'guest'],
+	},
+	age: {
+		type: Number,
+		min: 1,
+		max: 99,
+	},
+	numberOfArticles: {
+		type: Number,
+		default: 0,
+		min: 0,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		immutable: true,
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
+userSchema.pre('save', function (next) {
+	this.set({
+		updatedAt: new Date(),
+	});
+
+	this.fullName = `${this.firstName} ${this.lastName}`;
+	next();
+});
+
+userSchema.pre('validate', function (next) {
+	if (this.age < 0) {
+		this.age = 1;
+	}
+	next();
 });
 
 const User = mongoose.model('User', userSchema);
